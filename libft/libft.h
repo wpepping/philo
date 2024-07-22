d12ca72aab87ad28dc49975693690b4926eba85f
Wouter Pepping <wpepping@c3a4c6.42berlin.de>
2024-07-22 17:56:56 +0200

    .

---
+++ libft/libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wpepping <wpepping@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 17:05:31 by wpepping          #+#    #+#             */
+/*   Updated: 2024/07/19 13:14:53 by wpepping         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_isdigit(int c);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(long n, int fd);
+size_t	ft_strlen(const char *s);
+
+#endif
